dfc77b7049d090a30afd97627b3d9dfddbf39385
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2023-11-22 09:29:57 -0800

    Update TextInputForm.tsx

--- src/components/TextInputForm.tsx
+++ src/components/TextInputForm.tsx
@@ -19,7 +19,7 @@ export default function TextInputForm({ onSubmit }: TextInputFormProps) {
         onChange={(e) => setPrompt(e.target.value)}
         placeholder="Enter your prompt for image generation"
         rows={4}
-        className="border p-2 w-full"
+        className="border p-2 w-full text-black" // Added text-black class here
       />
       <button type="submit" className="bg-blue-500 text-white px-4 py-2 mt-2">
         Generate Image
